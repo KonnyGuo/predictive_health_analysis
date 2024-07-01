4379a3ad14b5360ddb323b6e7f28b22a144a81e8
KonnyGuo <guokonny@gmail.com>
2024-06-30 20:10:05 -0400

    edit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const patientRoutes = require("./routes/patientRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
